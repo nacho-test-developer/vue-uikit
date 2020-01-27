528d42c78aead12ba3ecea10eb734d10b381d8fb
yoelnacho <nacho.test.developer@gmail.com>
2020-01-26 22:58:37 -0300

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
@@ -16,13 +16,22 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+    // 1. Your custom variables and variable overwrites.
+    $global-link-color: #DA7D02;
+    // 2. Import default variables and available mixins.
+    @import "~uikit/src/scss/variables-theme.scss";
+    @import "~uikit/src/scss/mixins-theme.scss";
+    // 3. Your custom mixin overwrites.
+    @mixin hook-card() {
+        color: #000;
+    }
+    // 4. Import UIkit.
+    @import "~uikit/src/scss/uikit-theme.scss";
+    // Some font smoothing
+    html {
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-rendering: optimizeLegibility;
+    }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,92 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <nav class="uk-navbar-container" uk-navbar>
+      <div class="uk-navbar-left">
+
+          <div class="uk-navbar-item">
+              <form class="uk-search uk-search-navbar">
+                  <span uk-search-icon></span>
+                  <input class="uk-search-input" type="search" placeholder="Search...">
+              </form>
+          </div>
+
+      </div>
+    </nav>
+    <div class="uk-height-large uk-background-cover uk-light uk-flex" uk-parallax="bgy: -200" style="background-image: url('https://getuikit.com/docs/images/dark.jpg');">
+
+        <h1 class="uk-width-1-2@m uk-text-center uk-margin-auto uk-margin-auto-vertical">Headline</h1>
+
+    </div>
+
+    <div class="uk-container uk-margin">
+      <article class="uk-comment uk-comment-primary">
+        <header class="uk-comment-header uk-grid-medium uk-flex-middle" uk-grid>
+          <div class="uk-width-auto">
+            <img class="uk-comment-avatar" src="https://getuikit.com/docs/images/avatar.jpg" width="80" height="80" alt="">
+          </div>
+          <div class="uk-width-expand">
+            <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">Author</a></h4>
+            <ul class="uk-comment-meta uk-subnav uk-subnav-divider uk-margin-remove-top">
+              <li><a href="#">12 days ago</a></li>
+              <li><a href="#">Reply</a></li>
+            </ul>
+          </div>
+        </header>
+        <div class="uk-comment-body">
+          <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        </div>
+      </article>
+    </div>
+    <div class="uk-container uk-container-small uk-margin">
+      <div class="uk-flex uk-flex-center uk-margin">
+        <div class="uk-card uk-card-default uk-card-body">Item 1</div>
+        <div class="uk-card uk-card-default uk-card-body uk-margin-left">Item 2</div>
+        <div class="uk-card uk-card-default uk-card-body uk-margin-left">Item 3</div>
+      </div>
+        <div class="uk-child-width-1-3@m uk-grid-small uk-grid-match" uk-grid>
+          <div>
+              <div class="uk-card uk-card-default uk-card-body">
+                  <h3 class="uk-card-title">Default</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              </div>
+          </div>
+          <div>
+              <div class="uk-card uk-card-primary uk-card-body">
+                  <h3 class="uk-card-title">Primary</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              </div>
+          </div>
+          <div>
+              <div class="uk-card uk-card-secondary uk-card-body">
+                  <h3 class="uk-card-title">Secondary</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+              </div>
+          </div>
+      </div>
+    </div>
+
+    <div class="uk-container">
+      <article class="uk-article">
+        <progress class="uk-progress" :value="ok" max="100"></progress>
+        <!-- This is a button toggling the modal -->
+        <button class="uk-button uk-button-default uk-margin-small-right" type="button" uk-toggle="target: #modal-example">Open</button>
+
+        <!-- This is an anchor toggling the modal -->
+        <a href="#modal-example" uk-toggle>Open</a>
+      </article>
+    </div>
+
+    <!-- This is the modal -->
+    <div id="modal-example" uk-modal>
+      <div class="uk-modal-dialog uk-modal-body">
+          <h2 class="uk-modal-title">Headline</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          <p class="uk-text-right">
+              <button class="uk-button uk-button-default uk-modal-close" type="button">Cancel</button>
+              <button class="uk-button uk-button-primary" type="button">Save</button>
+          </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -35,24 +95,11 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      ok: '30'
+    }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
